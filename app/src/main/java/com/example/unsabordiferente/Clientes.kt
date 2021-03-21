e104c7955e1cb72eadb5aeaf4c119cabaf5e26cb
Rodrigo Gutierrez <rodrigo.gutierrez@up.ac.pa>
2021-03-21 18:14:01 -0500

    Agregamos los pedidos al RealtimeDatabase

---
+++ app/src/main/java/com/example/unsabordiferente/Clientes.kt
@@ -0,0 +1,4 @@
+package com.example.unsabordiferente
+
+class Clientes(var nombre:String, var cantidad:String,var celular:String,var ubicEntrega:String, var pedido:Boolean = true) {
+}
--- app/src/main/java/com/example/unsabordiferente/screens/Pedidos.kt
+++ app/src/main/java/com/example/unsabordiferente/screens/Pedidos.kt
@@ -1,25 +1,45 @@
 package com.example.unsabordiferente.screens
 
 import android.os.Bundle
+import android.util.Log
+
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.example.unsabordiferente.Clientes
 import com.example.unsabordiferente.R
 import com.example.unsabordiferente.databinding.FragmentPedidosBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class Pedidos : Fragment() {
     private lateinit var binding: FragmentPedidosBinding
+    val db = Firebase.database
+    val myRefPedidos = db.getReference("pedidos")
+    var celular: String = ""
+    var cantidad: String = ""
+    var nombre: String = ""
+    var ubicacion: String = ""
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle? ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_pedidos,container,false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pedidos, container, false)
 
 
         binding.btnAgregar.setOnClickListener {
-            Toast.makeText(context,"PEDIDO AGREGADO",Toast.LENGTH_LONG).show()
+            celular = binding.editCelular.text.toString()
+            cantidad = binding.editCantidad.text.toString()
+            nombre = binding.editNombre.text.toString()
+            ubicacion = binding.editUbic.text.toString()
+            if (celular.isNotBlank() and cantidad.isNotBlank() and nombre.isNotEmpty() and ubicacion.isNotBlank()) {
+                var cliente= Clientes(nombre,cantidad,celular,ubicacion)
+                myRefPedidos.child(cliente.celular).setValue(cliente)
+            }
+
         }
 
 
@@ -27,6 +47,8 @@ class Pedidos : Fragment() {
 
         return binding.root
     }
+
+    //funcion de obtener los datos del formulario
 
 
 }
